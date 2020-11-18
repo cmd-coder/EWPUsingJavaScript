a473982bae818c8fdc5abde1b5ea3f3ebf6066ea
CMD <gautamkailashsingh@gmail.com>
2020-11-18 22:04:50 +0530

    [Gautam] Calculated wages for an employee for 20 days using loop

--- EmployeeWageProblem.js
+++ EmployeeWageProblem.js
@@ -3,6 +3,7 @@ const IS_FULL_TIME=2;
 const PART_TIME_HOURS=4;
 const FULL_TIME_HOURS=8;
 const WAGE_PER_HOUR=20;
+const NUM_OF_WORKING_DAYS=20;
 
 function getWorkingHours(empCheck)
 {
@@ -15,11 +16,17 @@ function getWorkingHours(empCheck)
             return FULL_TIME_HOURS;
             break;
         default:
-            empHrs=0;
+            return 0;
     }
 }
+
+
+
 let empHrs=0;
-let empCheck=Math.floor(Math.random()*10)%3;
-empCheck=getWorkingHours(empCheck);
-let empWage=empCheck*WAGE_PER_HOUR;
+for(let day=0;day<NUM_OF_WORKING_DAYS;day++)
+{
+    let empCheck = Math.floor(Math.random()*10)%3;
+    empHrs+=getWorkingHours(empCheck);
+}
+let empWage=empHrs*WAGE_PER_HOUR;
 console.log("Emp Wage: "+empWage);
