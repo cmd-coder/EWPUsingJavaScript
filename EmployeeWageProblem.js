e998349bf70bb708b1147922d632dbe0fea05379
CMD <gautamkailashsingh@gmail.com>
2020-11-18 21:55:03 +0530

    [Gautam] Calculate daily wages using functions

--- EmployeeWageProblem.js
+++ EmployeeWageProblem.js
@@ -4,19 +4,22 @@ const PART_TIME_HOURS=4;
 const FULL_TIME_HOURS=8;
 const WAGE_PER_HOUR=20;
 
-let empHrs=0;
-empCheck=Math.floor(Math.random()*10)%3;
-switch(empCheck)
+function getWorkingHours(empCheck)
 {
-    case IS_PART_TIME:
-        empHrs=PART_TIME_HOURS;
-        break;
-    case IS_FULL_TIME:
-        empHrs=FULL_TIME_HOURS;
-        break;
-    default:
-        empHrs=0;
+    switch(empCheck)
+    {
+        case IS_PART_TIME:
+            return PART_TIME_HOURS;
+            break;
+        case IS_FULL_TIME:
+            return FULL_TIME_HOURS;
+            break;
+        default:
+            empHrs=0;
+    }
 }
-
-let empWage=empHrs*WAGE_PER_HOUR;
+let empHrs=0;
+let empCheck=Math.floor(Math.random()*10)%3;
+empCheck=getWorkingHours(empCheck);
+let empWage=empCheck*WAGE_PER_HOUR;
 console.log("Emp Wage: "+empWage);
